2cae37b16c91fb064b402468dc454cdcab28dce1
Mattie <78979239+Mattie1391@users.noreply.github.com>
2025-04-19 20:08:35 +0800

    Initial commit

---
+++ config/data-source.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["./models/**/*.js"],
+  synchronize: true,
+  logging: false
+};
